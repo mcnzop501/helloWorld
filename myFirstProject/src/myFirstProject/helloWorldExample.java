113d5c1d4e0d389276655da9daf4bd58426747b6
mcnzop501 <mcnzop501@gmail.com>
2020-02-25 21:30:53 +1300

    Try 4

--- myFirstProject/src/myFirstProject/helloWorldExample.java
+++ myFirstProject/src/myFirstProject/helloWorldExample.java
@@ -4,7 +4,7 @@ public class helloWorldExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World MoFo #2");
+		System.out.println("Hello World MoFo #504");
 	}
 
 }
