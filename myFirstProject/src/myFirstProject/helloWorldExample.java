88e46c546a8d72ab2b74711aada8faa7d36f15c5
mcnzop501 <mcnzop501@gmail.com>
2020-02-25 21:05:47 +1300

    Initial setup

---
+++ myFirstProject/src/myFirstProject/helloWorldExample.java
@@ -0,0 +1,10 @@
+package myFirstProject;
+
+public class helloWorldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
